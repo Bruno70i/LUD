c2fa18710353161e6ad646f4074b8c3cb95a2c97
bruno70i <bruno.npsilva70@gmail.com>
2025-03-23 21:40:13 -0300

    Novas anterações
    pequenas mudança no projeto

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 
-var foto = document.getElementById('foto-wallpaper')
-foto.addEventListener('mouseover', () => {
-    foto.src = 'img/wallpaper/foto_wallpaper_intro_black.jpg';
-
-    
-    foto.addEventListener('mouseout', () => {
-        foto.src = 'img/wallpaper/foto_wallpaper_intro_white.jpg';
-    });
-
-})
